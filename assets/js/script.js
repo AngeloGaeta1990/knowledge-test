cfd30654558aae989b0549fc5d7df4e908ba5fcb
Angelo Gaeta <131547377+AngeloGaeta1990@users.noreply.github.com>
2023-08-18 19:05:53 +0100

    feat: adds home link to exit button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,26 @@
-console.log("stocazzo")
+document.addEventListener("DOMContentLoaded",function() {
+    let buttons = document.getElementsByTagName('button');
+    
+    for (let button of buttons) {
+        button.addEventListener('click', function() { 
+    
+            if (this.getAttribute("data-type") === "submit"){
+                checkAnswer();
+            } else {
+                let gameType = this.getAttribute("data-type"); {
+                   runGame(gameType);
+                }
+            }
+
+        })
+    }
+    document.getElementById("answer-box").addEventListener("keydown", function(event){
+        if (event.key === "Enter"){
+            checkAnswer();
+        }
+
+
+    })
+
+    runGame("addition");
+})
