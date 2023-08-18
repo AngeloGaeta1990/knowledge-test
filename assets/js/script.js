1e6ec6562387eb2089a905cc192ffa7dd5702654
Angelo Gaeta <131547377+AngeloGaeta1990@users.noreply.github.com>
2023-08-18 20:13:53 +0100

    feat: selects a random question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,61 @@
-document.addEventListener("DOMContentLoaded",function() {
-    let buttons = document.getElementsByTagName('button');
-    
-    for (let button of buttons) {
-        button.addEventListener('click', function() { 
-    
-            if (this.getAttribute("data-type") === "submit"){
-                checkAnswer();
-            } else {
-                let gameType = this.getAttribute("data-type"); {
-                   runGame(gameType);
-                }
-            }
 
-        })
+// List of questions objects to show in the quiz
+let questions = [
+    {
+       id: 1, 
+       category: "history",
+       question: 'Who pronounced the famous quotation "Alea jacta est"?',
+       answer1: "Julius Caesar",
+       answer2: "Caligola",
+       answer3: "Nerone",
+       answer4: "Gaius Octavius, Augustus",
+       correctAnswer: "Julius Caesar",
+    },
+    {
+        id:2,
+        category: "entertainment",
+        question: "Who won game of the year in 2022?",
+        answer1: "Sekiro",
+        answer2: "God of War: Ragnarok",
+        answer3: "Elden ring",
+        answer4: "It takes two",
+        correctAnswer: "Elden ring",
+
     }
-    document.getElementById("answer-box").addEventListener("keydown", function(event){
-        if (event.key === "Enter"){
-            checkAnswer();
-        }
+];
 
 
-    })
+// list of answered questions
+let questionAsked = [];
 
-    runGame("addition");
-})
+// total number of questions
+const totalQuestions = 30
+
+
+
+
+
+// document.addEventListener("DOMContentLoaded",function() {
+//     let buttons = document.getElementsByTagName('button');
+    
+//     for (let button of buttons) {
+//         button.addEventListener('click', function() { 
+    
+//             if (this.getAttribute("data-type") === "start"){
+//                 checkAnswer();
+//             }
+//         }
+// )}
+// })
+
+
+/*
+* Selects a random question from the list of arrays questions
+*/
+function selectRandomQuestion() {
+    let currentQuestionIndex = Math.floor(Math.random() * questions.length);
+    currentQuestion = questions[currentQuestionIndex];
+}
+
+
+selectRandomQuestion()
