dc422fd8e7f5f46620d75834292c3b744a30da99
Angelo Gaeta <131547377+AngeloGaeta1990@users.noreply.github.com>
2023-08-19 23:49:20 +0100

    feat: adds main function and next event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,59 +3,112 @@
 let questions = [
     {
        id: 1 , 
-       category: "history",
+       category: "History",
        question: 'Who pronounced the famous quotation "Alea jacta est"?',
        answer1: "Julius Caesar",
        answer2: "Caligola",
        answer3: "Nerone",
        answer4: "Gaius Octavius, Augustus",
        correctAnswer: "Julius Caesar",
+       correctAnswerId: "option1",
     },
     {
         id:2 ,
-        category: "entertainment",
+        category: "Entertainment",
         question: "Who won game of the year in 2022?",
         answer1: "Sekiro",
         answer2: "God of War: Ragnarok",
         answer3: "Elden ring",
         answer4: "It takes two",
         correctAnswer: "Elden ring",
+        correctAnswerId: "option3",
 
     },
 
     {
         id: 3 ,
-        category: "geography" ,
+        category: "Geography" ,
         question: "Which of the following is the capital of Norway?",
         answer1: "Rome",
         answer2: "Oslo",
         answer3: "Prague",
         answer4: "Nairobi",
         correctAnswer: "Oslo",
+        correctAnswerId: "option2",
 
     }
 ];
 
+//List of categories with scores and times
+let categories = [
+    {
+        name: "History",
+        time: 0,
+        score: 0,
+    },
+
+    {
+        name: "Geography",
+        time: 0,
+        score: 0,
+    },
+
+    {
+        name: "Entertainment",
+        time: 0,
+        score: 0,
+    }
+
+]
+
+let questionCounter = 1
 
 // total number of questions
 const totalQuestions = 30
 
-
-//Generates fisrt question
-generateFirstQuestion() 
-//adds and update the timer
-startTimer()
-updateTimer()
-
-
-
 document.addEventListener("DOMContentLoaded",function() {
 
+    //Generates fisrt question
+    question = generateQuestion()
+    // Displays question counter
+    document.getElementById("question-counter").textContent = questionCounter
 
-    let buttons = document.getElementsByTagName('button');
-    
+    //adds and update the timer
+    startTime = logTime()
+    startTimer()
+    updateTimer()
+    document.getElementById("next").addEventListener("click", function() {
+        // Logs time after answer has been selected
+        endTime = logTime()
+        // Get the radiobutton id of selected anwer
+        let selectedAnswer = document.querySelector('input[name="answer"]:checked');
+        // checks if id of selected radiobutton matches with the correctAnswerId attribute of question object
+        if (selectedAnswer.value === question.correctAnswerId) {
+            // find thwe category object which name matches question catergoty attribute
+            for (category of categories) {
+                if (category.name === question.category){
+                // if answer is correct increase category score and incerease category time
+                category.score++
+                category.time =+ Math.floor(((endTime - startTime)% 60000)/1000)
+                }
+            }
+        }
+        stopTimer()
+        deleteQuestion(question)
+        // Updates and displays question counter
+        questionCounter++
+        document.getElementById("question-counter").textContent = questionCounter
+        let newQuestion = generateQuestion()
 
-})
+        }
+        )
+
+    })
+
+
+
+
+
     
 //     for (let button of buttons) {
 //         button.addEventListener('click', function() { 
@@ -89,10 +142,10 @@ function deleteQuestion(currentQuestion) {
 }
 
 /*
-*Generates the first question
+*Generates a question
 */
 
-function generateFirstQuestion() {
+function generateQuestion() {
     let question = selectRandomQuestion();
     document.getElementById('category').textContent = question["category"];
     document.getElementById('question').textContent = question["question"];
@@ -100,8 +153,14 @@ function generateFirstQuestion() {
     document.getElementById('answer2').textContent = question["answer2"];
     document.getElementById('answer3').textContent = question["answer3"];
     document.getElementById('answer4').textContent = question["answer4"];
+    return question
     }
     
+
+
+   /*
+*Generates the a new que question
+*/ 
 
 
 //Timer functions
@@ -125,8 +184,7 @@ function updateTimer() {
     let elapsedTime = currentTime - startTime;
     
     let minutes = Math.floor(elapsedTime / 60000);
-    let seconds = Math.floor((elapsedTime % 60000) / 1000);
-    
+    let seconds = Math.floor((elapsedTime % 60000) / 1000);   
     let formattedTime = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
     
     document.getElementById('timer').textContent = formattedTime;
@@ -137,4 +195,12 @@ function updateTimer() {
 */  
   function stopTimer() {
     clearInterval(timerInterval);
+  }
+
+
+  /*
+  * Records the actual time
+  */
+  function logTime () {
+    return Date.now()
   }
