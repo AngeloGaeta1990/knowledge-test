9a8af1ae4e6e6c8929362c902c051877a236e0bd
Angelo Gaeta <131547377+AngeloGaeta1990@users.noreply.github.com>
2023-08-23 21:00:09 +0100

    feat: added all questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,20 +13,7 @@ let questions = [
        correctAnswerId: "option1",
     },
     {
-        id:2 ,
-        category: "Entertainment",
-        question: "Who won game of the year in 2022?",
-        answer1: "Sekiro",
-        answer2: "God of War: Ragnarok",
-        answer3: "Elden ring",
-        answer4: "It takes two",
-        correctAnswer: "Elden ring",
-        correctAnswerId: "option3",
-
-    },
-
-    {
-        id: 3 ,
+        id: 2 ,
         category: "Geography" ,
         question: "Which of the following is the capital of Norway?",
         answer1: "Rome",
@@ -35,6 +22,23 @@ let questions = [
         answer4: "Nairobi",
         correctAnswer: "Oslo",
         correctAnswerId: "option2",
+
+
+       
+
+    },
+
+    { 
+        id:3 ,
+        category: "Entertainment",
+        question: "Who won game of the year in 2022?",
+        answer1: "Sekiro",
+        answer2: "God of War: Ragnarok",
+        answer3: "Elden ring",
+        answer4: "It takes two",
+        correctAnswer: "Elden ring",
+        correctAnswerId: "option3",
+        
 
     },
 
@@ -86,19 +90,7 @@ let questions = [
     },
 
     {
-       id: 8,
-       category: "Entertainment",
-       question: "In which of the following bands the singer commited suicide?",
-       answer1: "30 seconds to Mars",
-       answer2: "Of Monsters and Man",
-       answer3: "Imagine Dragons",
-       answer4: "Nirvana",
-       correctAnswer: "Nirvana",
-       correctAnswerId: "option4",
-    },
-
-    {
-        id: 9,
+        id: 8,
         category: "Geography",
         question: "Which one is longest river in the world?",
         answer1: "Rio",
@@ -107,6 +99,19 @@ let questions = [
         answer4: "Mississipi",
         correctAnswer: "Nile",
         correctAnswerId: "option2",
+
+    },
+
+    {
+       id: 9,
+       category: "Entertainment",
+       question: "In which of the following bands the singer commited suicide?",
+       answer1: "30 seconds to Mars",
+       answer2: "Of Monsters and Man",
+       answer3: "Imagine Dragons",
+       answer4: "Nirvana",
+       correctAnswer: "Nirvana",
+       correctAnswerId: "option4",
      },
 
      {
@@ -159,6 +164,18 @@ let questions = [
 
      {
         id: 14,
+        category: "Geography",
+        question: "What is the geographical location of the Earth's lowest point?",
+        answer1: "Mariana Trench",
+        answer2: "Mount Everest",
+        answer3: "Great Barrier Reef",
+        answer4: "Amazon Rainforest",
+        correctAnswer: "Mariana Trench",
+        correctAnswerId: "option1",
+     },
+
+     {
+        id: 15,
         category: "Entertainment",
         question: "In Which of the following movies Quentin Tarantino was not the director",
         answer1: "The Hateful Eight",
@@ -167,17 +184,201 @@ let questions = [
         answer4: "Django Unchained",
         correctAnswer: "Eyes Wide Shut",
         correctAnswerId: "option3",
+       
      },
 
      {
-        id: 15,
+        id: 16,
+        category: "Science",
+        question: "What is the chemical formula for magnesium chloride, and how does it represent the composition of the compound?",
+        answer1: "MgCl",
+        answer2: "MgCl2",
+        answer3: "Mg2Cl",
+        answer4: "Mg2Cl2",
+        correctAnswer: "MgCl2",
+        correctAnswerId: "option2",
+
+     },
+
+     {
+        id: 17,
+        category: "Sport",
+        question: "Which Formula 1 driver holds the record for the most World Drivers' Championship titles?",
+        answer1: "Lewis Hamilton",
+        answer2: "Michael Schumacher",
+        answer3: "Ayrton Senna",
+        answer4: " Sebastian Vettel",
+        correctAnswer: "Michael Schumacher",
+        correctAnswerId: "option2",
+
+     },
+
+     {
+        id: 18,
+        category: "Literature",
+        question: "Who is the author of the novel '1984,' a dystopian classic that explores themes of government control and surveillance?",
+        answer1: "George Orwell",
+        answer2: "Aldous Huxley",
+        answer3: "Ray Bradbury",
+        answer4: "J.D. Salinger",
+        correctAnswer: "George Orwell",
+        correctAnswerId: "option1",
+
+     },
+
+     {
+        id: 19,
+        category: "History",
+        question: "The ancient city of Rome is said to have been founded by twin brothers Romulus and Remus. According to legend, they were raised by which animal?",
+        answer1: "Lion",
+        answer2: "Wolf",
+        answer3: "Bear",
+        answer4: "Eagle",
+        correctAnswer: "Wolf",
+        correctAnswerId: "option2",
+
+     },
+
+     {
+
+        id: 20,
+        category: "Geograpghy",
+        question: "Which continent is known as the 'Land Down Under'?",
+        answer1: "Africa",
+        answer2: "South America",
+        answer3: "Australia",
+        answer4: "Antarctica",
+        correctAnswer: "Australia",
+        correctAnswerId: "option3",
+      
+     },
+
+     {
+        id: 21,
+        category: "Entertainment",
+        question: "Which actor portrayed the character Tony Stark, also known as Iron Man, in the Marvel Cinematic Universe?",
+        answer1: "Chris Evans",
+        answer2: "Robert Downey Jr.",
+        answer3: "Chris Hemsworth",
+        answer4: "Mark Ruffalo",
+        correctAnswer: "Robert Downey Jr.",
+        correctAnswerId: "option2",
+
+
+     },
+     {
+        id: 22,
+        category: "Science",
+        question: "What type of electromagnetic waves have the shortest wavelength and highest energy?",
+        answer1: "Infrared",
+        answer2: "Ultraviolet",
+        answer3: "Microwave",
+        answer4: "Radio waves",
+        correctAnswer: "Ultraviolet",
+        correctAnswerId: "option2",
+
+     },
+     {
+        id: 23,
+        category: "Sport",
+        question: "In which sport would you perform a slam dunk?",
+        answer1: "Soccer(Football)",
+        answer2: "Baseball",
+        answer3: "Basketball",
+        answer4: "Tennis",
+        correctAnswer: "Basketball",
+        correctAnswerId: "option3",
+
+     },
+
+     {
+        id: 24,
+        category: "Literature",
+        question: "Who wrote the novel 'Pride and Prejudice'?",
+        answer1: "Jane Austen",
+        answer2: "Charlotte Brontë",
+        answer3: "Emily Dickinson",
+        answer4: " Virginia Woolf",
+        correctAnswer: "Jane Austen",
+        correctAnswerId: "option1",
+
+     },
+
+     {
+        id: 25,
+        category: "History",
+        question: "The Renaissance was a period of cultural and artistic flourishing that originated in which European city?",
+        answer1: "Athens",
+        answer2: "Rome",
+        answer3: "Paris",
+        answer4: "Florence",
+        correctAnswer: "Florence",
+        correctAnswerId: "option4",
+
+     },
+
+     
+     {
+        id: 26,
         category: "Geography",
-        question: "What is the geographical location of the Earth's lowest point?",
-        answer1: "Mariana Trench",
-        answer2: "Mount Everest",
-        answer3: "Great Barrier Reef",
-        answer4: "Amazon Rainforest",
-        correctAnswer: "Mariana Trench",
+        question: "Which mountain range stretches along the western coast of South America, spanning through several countries including Peru, Bolivia, and Chile?",
+        answer1: "Rocky Mountains",
+        answer2: "Himalayas",
+        answer3: "Alps",
+        answer4: "Andes",
+        correctAnswer: "Andes",
+        correctAnswerId: "option4",
+
+     },
+
+     {
+        id: 27,
+        category: "Entertainment",
+        question: "In which of the following games the comander Shepard is the main character?",
+        answer1: "Dragon age origins",
+        answer2: "Dark Souls",
+        answer3: "Baldurs Gate 3",
+        answer4: "Mass effect 2",
+        correctAnswer: "Mass effect 2",
+        correctAnswerId: "option4",
+
+     },
+
+     {
+        id: 28,
+        category: "Science",
+        question: "What is the process by which a solid changes directly into a gas without passing through the liquid phase?",
+        answer1: "Sublimation",
+        answer2: "Evaporation",
+        answer3: "Condensation",
+        answer4: "Melting",
+        correctAnswer: "Sublimation",
+        correctAnswerId: "option1",
+
+     },
+
+     {
+        id: 29,
+        category: "Sport",
+        question: "Which city is famous for hosting the Summer Olympics in 2008 and is known for its iconic 'Bird's Nest' stadium?",
+        answer1: "Tokyo",
+        answer2: "Rio de Janeiro",
+        answer3: "Beijing",
+        answer4: "Sydney",
+        correctAnswer: "Beijing",
+        correctAnswerId: "option3",
+
+     },
+
+     {
+        id: 30,
+        category: "Literature",
+        question: "Which of the following book is part of the 'A Song of Ice and Fire' saga?",
+        answer1: "A Storm of Swords",
+        answer2: "The Wolves Kingdom",
+        answer3: "The Deads Laughs",
+        answer4: "The Cursed Sanctuary",
+        correctAnswer: "A Storm of Swords",
         correctAnswerId: "option1",
      },
 ];
@@ -291,7 +492,7 @@ document.addEventListener("DOMContentLoaded",function() {
         }
         document.getElementById('total-score').textContent = totalScore
         document.getElementById('total-time').textContent = totalTime
-        radarPlot(categories)
+        radarPlot(storedCategories)
     }});
         
 
