428487cd86d95c1d091a2e93053d34537f05a84a
Angelo Gaeta <131547377+AngeloGaeta1990@users.noreply.github.com>
2023-08-26 21:05:38 +0100

    style: radarpot label size

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
-import {questions} from './questions.js'
+// Imports list of questions for questions.js file
+import { questions } from './questions.js'
 
 
 //List of categories with scores and times
@@ -7,7 +8,7 @@ let categories = [
         name: "History",
         time: 0,
         score: 0,
-    },        
+    },
 
     {
         name: "Geography",
@@ -56,8 +57,8 @@ let startQuestionTime;
 
 
 
-document.addEventListener("DOMContentLoaded",function() {
-        if (window.location.href.endsWith("questions.html")) {
+document.addEventListener("DOMContentLoaded", function () {
+    if (window.location.href.endsWith("questions.html")) {
         //Generates first question
         question = generateQuestion()
         // Displays question counter
@@ -73,53 +74,55 @@ document.addEventListener("DOMContentLoaded",function() {
 
 
 
-        document.getElementById("next").addEventListener("click", function() {
+        document.getElementById("next").addEventListener("click", function () {
             nextQuestion()
         })
         // And for Enter keyboard event
-        document.getElementById("answers").addEventListener("keydown", function(event) {     
+        document.getElementById("answers").addEventListener("keydown", function (event) {
             if (event.key === "Enter") {
-            nextQuestion()
+                nextQuestion()
             }
         })
 
-        
-    }});
-            
-document.addEventListener("DOMContentLoaded",function() {
-    if (window.location.href.endsWith("results.html")){
+
+    }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    if (window.location.href.endsWith("results.html")) {
         let storedCategories = JSON.parse(localStorage.getItem('categories'))
         let totalScore = 0
         let totalTime = 0
-        for (let category of storedCategories) { 
+        for (let category of storedCategories) {
             document.getElementById(category.name.toLowerCase()).textContent = category.score;
             document.getElementById(category.name.toLowerCase() + "-time").textContent = category.time;
             totalScore += category.score
-            totalTime  += category.time
+            totalTime += category.time
         }
         document.getElementById('total-score').textContent = totalScore
         document.getElementById('total-time').textContent = totalTime
         radarPlot(storedCategories)
-    }});
-        
-
-                
-                
-                
-    
-
-
-       
+    }
+});
 
 
 
 
 
 
-    
+
+
+
+
+
+
+
+
+
+
 //     for (let button of buttons) {
 //         button.addEventListener('click', function() { 
-    
+
 //             if (this.getAttribute("data-type") === "start"){
 //                 checkAnswer();
 //             }
@@ -144,7 +147,7 @@ function deleteQuestion(currentQuestion) {
     let currentQuestionIndex = questions.indexOf(currentQuestion);
     // checks if the element is in the array
     if (currentQuestionIndex !== -1) {
-        questions.splice(currentQuestionIndex,1);
+        questions.splice(currentQuestionIndex, 1);
     }
 }
 
@@ -160,8 +163,16 @@ function generateQuestion() {
     document.getElementById('answer2').textContent = question["answer2"];
     document.getElementById('answer3').textContent = question["answer3"];
     document.getElementById('answer4').textContent = question["answer4"];
-    return question}
-    
+
+
+    // Clear radio button selection
+    let radioButtons = document.querySelectorAll('input[name="answer"]');
+    radioButtons.forEach(radioButton => {
+        radioButton.checked = false;
+    });
+    return question
+}
+
 
 
 //Timer functions
@@ -172,8 +183,9 @@ function generateQuestion() {
 function startTimer() {
     startTime = Date.now();
     updateTimer(); // Update timer initially
-    timerInterval = setInterval(function() {
-    updateTimer()}, 1000); // Update every second
+    timerInterval = setInterval(function () {
+        updateTimer()
+    }, 1000); // Update every second
 }
 
 /*
@@ -183,41 +195,41 @@ function startTimer() {
 function updateTimer() {
     let currentTime = Date.now();
     let elapsedTime = currentTime - startTime;
-    
+
     let minutes = Math.floor(elapsedTime / 60000);
-    let seconds = Math.floor((elapsedTime % 60000) / 1000);   
+    let seconds = Math.floor((elapsedTime % 60000) / 1000);
     let formattedTime = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
-    
+
     document.getElementById('timer').textContent = formattedTime;
-  }
+}
 
 /*
 * Ends the timer
-*/  
-  function stopTimer() {
+*/
+function stopTimer() {
     clearInterval(timerInterval);
     // Reset timerInterval to undefined after stopping
     timerInterval = undefined;
-  }
+}
 
 
-  /*
-  * Records the actual time
-  */
-  function logTime () {
+/*
+* Records the actual time
+*/
+function logTime() {
     return Date.now()
-  }
+}
 
 /*
 *Adds exit event function
 */
 function exitGameButton() {
-    document.getElementById("exit").addEventListener("click", function() {
+    document.getElementById("exit").addEventListener("click", function () {
         let confirmExit = confirm("Are you sure you want to leave the game?");
         if (confirmExit) {
             alert("Thank you for playing!")
             let homepageLink = document.getElementById("exit-link")
-                homepageLink.href= "index.html"
+            homepageLink.href = "index.html"
         }
     })
 }
@@ -236,68 +248,68 @@ function exitGameButton() {
 * Picks new question from the array 
 * if the question counter is > total questions then it loads results page
 */
-function nextQuestion () {
-        //stops the timer
-        stopTimer();
-        // Logs time 
-        // Get the radiobutton id of selected anwer`
-        let selectedAnswer = document.querySelector('input[name="answer"]:checked');
-        // checks if id of selected radiobutton matches with the correctAnswerId attribute of question object
-        // Record the time when the user selects an answer
-        let endQuestionTime = logTime();
-        let timeDifferenceInSeconds = Math.floor((endQuestionTime - startQuestionTime) / 1000);
-        console.log(question)
-        for (let category of categories) {
-            console.log(category)
+function nextQuestion() {
+    //stops the timer
+    stopTimer();
+    // Logs time 
+    // Get the radiobutton id of selected anwer`
+    let selectedAnswer = document.querySelector('input[name="answer"]:checked');
+    // checks if id of selected radiobutton matches with the correctAnswerId attribute of question object
+    // Record the time when the user selects an answer
+    let endQuestionTime = logTime();
+    let timeDifferenceInSeconds = Math.floor((endQuestionTime - startQuestionTime) / 1000);
+    console.log(question)
+    for (let category of categories) {
+        console.log(category)
         // Finds the category object which name matches question catergoty attribute
-            if (category.name === question["category"]){
-                // incerease time score in catagory object (results)
-                category.time += timeDifferenceInSeconds
-                // if answer is correct increase category score 
-                if (selectedAnswer.value === question.correctAnswerId){
-                    category.score++
-                }
+        if (category.name === question["category"]) {
+            // incerease time score in catagory object (results)
+            category.time += timeDifferenceInSeconds
+            // if answer is correct increase category score 
+            if (selectedAnswer.value === question.correctAnswerId) {
+                category.score++
             }
         }
-        questionCounter++;
-        // Deletes question from array of questions
-        deleteQuestion(question);
-        //Generates new question question
-        document.getElementById("question-counter").textContent = questionCounter
-        if (questionCounter <= totalQuestions) {
-             // Generates first question
-            question = generateQuestion();
-            // Update startQuestionTime with current time
-            startQuestionTime = logTime();
-            // Displays question counter
-            startTimer();
-            updateTimer();
-        } else {
-            // if the question counter is > total questions loads results page 
-            gameOver()
+    }
+    questionCounter++;
+    // Deletes question from array of questions
+    deleteQuestion(question);
+    //Generates new question question
+    document.getElementById("question-counter").textContent = questionCounter
+    if (questionCounter <= totalQuestions) {
+        // Generates first question
+        question = generateQuestion();
+        // Update startQuestionTime with current time
+        startQuestionTime = logTime();
+        // Displays question counter
+        startTimer();
+        updateTimer();
+    } else {
+        // if the question counter is > total questions loads results page 
+        gameOver()
 
-        
-        }
-                }
-            
+
+    }
+}
+
 
 /*
 *Ends the game
 */
-function gameOver () {
+function gameOver() {
     // Store categories' scores in local storage
     localStorage.setItem('categories', JSON.stringify(categories));
     // Redirect to results.html
     window.location.href = "results.html";
     let resultLink = document.getElementById("result-link");
-    resultLink.href= "results.html";
-    }
+    resultLink.href = "results.html";
+}
 
 
 /*
 * Draws a radar plot of the scores
 */
-function radarPlot (categories) {
+function radarPlot(categories) {
     // Extract data
     let subjectNames = categories.map(category => category.name);
     let scores = categories.map(category => category.score);
@@ -305,41 +317,64 @@ function radarPlot (categories) {
     let canvas = document.getElementById('radar-plot');
     let ctx = canvas.getContext('2d');
     let chart = new Chart(ctx, {
-    type: 'radar',
-    data: {
-        labels: subjectNames,
-        datasets: [{
-        label: 'Scores',
-        data: scores,
-        borderColor: '#ba3838',
-        backgroundColor: 'rgba(186, 56, 56, 0.25)',
-        pointBackgroundColor: 'rgba(186, 56, 56, 0.85)',
-        pointBorderColor: '#fff',
-        pointHoverBackgroundColor: '#fff',
-        pointHoverBorderColor: 'rgba(75, 192, 192, 1)',
-        }],
-    },
-    options: {
-        scale: {
-            pointLabels: {
-                fontSize: 14, // Adjust the font size as needed
-                fontWeight: 'bold', // Set the font weight to 'bold'
+        type: 'radar',
+        data: {
+            labels: subjectNames,
+            datasets: [{
+                label: 'Total Score',
+                data: scores,
+                borderColor: '#ba3838',
+                backgroundColor: 'rgba(186, 56, 56, 0.25)',
+                pointBackgroundColor: 'rgba(186, 56, 56, 0.85)',
+                pointBorderColor: '#fff',
+                pointHoverBackgroundColor: '#fff',
+                pointHoverBorderColor: 'rgba(75, 192, 192, 1)',
+            }],
+        },
+        options: {
+            scale: {
+                ticks: {
+                    beginAtZero: true,
+                    max: 5, // Adjust the maximum value as needed
+                    stepSize: 1,
+                },
+
+                gridLines: {
+                    circular: true,
+                  
+                },
+            
+                elements: {
+                    line: {
+                      borderWidth:500// this will make the lines thicker
+                    }
+                  },
+            
             },
-        ticks: {
-            beginAtZero: true,
-            max: 5, // Adjust the maximum value as needed
-            fontSize: 5, // Adjust the font size as needed
-        },
-        angleLines: {
-            color: 'rgba(0, 0, 0, 1)', // Adjust the color of angle lines
-            lineWidth: 10, // Adjust the line width of angle lines
-        },
-        gridLines: {
-            circular: true,
-        },
-    },
- 
-    
-    }
-})};
-     
+            scales: {
+                r: {
+                    pointLabels: {
+                        font: {
+                            size: "13em",
+                            family: "'Lumanosimo', 'sans-serif'",
+                            weight: "bold",
+                            color: "black",
+                            
+                        }
+                    },
+
+                }
+            },
+            plugins: {
+                legend: {
+                    labels: {
+                        font: {
+                            size: "13em", // Adjust the font size of the legend labels
+                            family: "'Lumanosimo', 'sans-serif'",
+                            weight: "bold",
+                        },
+                    }
+                },
+            }
+    },})
+};
